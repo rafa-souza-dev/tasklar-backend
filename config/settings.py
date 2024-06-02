c62b9249eb6828ba5f7eda5a4c1a0242a0c8f078
Rafael Souza <rafael.jose@ferreri.co>
2024-06-02 18:02:06 -0300

    add mysql config

--- config/settings.py
+++ config/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT')
     }
 }
 
