92fb968f76680c82e377af124021adedf91754d9
Rafael Souza <rafael.jose@ferreri.co>
2024-06-19 00:28:05 -0300

    change mysqlclient version

--- config/settings.py
+++ config/settings.py
@@ -91,8 +91,8 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT')
+        'HOST': config('DB_HOST', 'db'),
+        'PORT': config('DB_PORT', '3306')
     }
 }
 
