95e1b1d7f579ed446943fea411ae2c52163c9dbd
Rafael Souza <rafael.jose@ferreri.co>
2024-07-17 23:57:17 -0300

    setup for test list service

--- service/test/test_list_services.py
+++ service/test/test_list_services.py
@@ -3,8 +3,10 @@ from django.urls import reverse
 from rest_framework.test import APITestCase
 
 from authentication.models import User
+from consumer.models import Consumer
 from job.models import Category, Job
 from service.models import Service
+from tasker.models import Tasker
 
 
 class TestListServices(APITestCase):
@@ -13,17 +15,18 @@ class TestListServices(APITestCase):
         self.tasker_password = 'S6y6@9iP5Q&r5BQx'
         self.consumer_email = 'consumer@test.com'
         self.consumer_password = 'S6y6@9iP5Q&r5BQx'
-        self.hashed_tasker_password = make_password(self.password)
-        self.hashed_consumer_password = make_password(self.password)
+        self.hashed_tasker_password = make_password(self.tasker_password)
+        self.hashed_consumer_password = make_password(self.consumer_password)
 
-        tasker = User.objects.create(email=self.tasker_email, password=self.hashed_tasker_password, profile_type='T')
-        consumer = User.objects.create(email=self.consumer_email, password=self.hashed_consumer_password, profile_type='C')
+        user_tasker = User.objects.create(email=self.tasker_email, password=self.hashed_tasker_password, profile_type='T')
+        user_consumer = User.objects.create(email=self.consumer_email, password=self.hashed_consumer_password, profile_type='C')
+        tasker = Tasker.objects.create(user=user_tasker)
+        consumer = Consumer.objects.create(user=user_consumer)
         category = Category.objects.create(name='Faxina')
         job = Job.objects.create(
-            tasker=tasker, category=category, contact='123', 
-            value=100, description='muito bão', duration='1hr30min',
-            days_of_week='0001110', description='muito bão', duration='1hr30min',
-            start_time='06:00:00', end_time='19:00:00'
+            tasker=tasker, category=category, contact='123',
+            value=100, days_of_week='0001110', description='muito bão',
+            duration='1hr30min', start_time='06:00:00', end_time='19:00:00',
         )
         Service.objects.create(
            consumer=consumer, job=job, tasker=tasker,
