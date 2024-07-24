b0566027a514ba19a527ed7aba2ab7a364a493f5
Rafael Souza <rafael.jose@ferreri.co>
2024-07-24 01:19:32 -0300

    create JobDetailsCategorySerializer serializer

--- service/api/serializers.py
+++ service/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from consumer.api.serializers import ConsumerSerializer
+from job.api.serializers import JobDetailsCategorySerializer
 from service.models import Service
 
 class ServiceSerializer(serializers.ModelSerializer):
@@ -10,6 +11,7 @@ class ServiceSerializer(serializers.ModelSerializer):
 
 class ServiceFullSerializer(serializers.ModelSerializer):
     consumer = ConsumerSerializer()
+    job = JobDetailsCategorySerializer()
 
     class Meta:
         model = Service
